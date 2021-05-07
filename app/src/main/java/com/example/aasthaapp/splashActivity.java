639869f43a98eb7bea55f12243abb80e87f4909f
Akriti-05 <akritiprasun@gmail.com>
2021-05-08 00:41:13 +0530

    splash

---
+++ app/src/main/java/com/example/aasthaapp/splashActivity.java
@@ -0,0 +1,38 @@
+package com.example.aasthaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+
+                }
+                finally {
+                    Intent intent = new Intent(splashActivity.this, SignInActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+
+    }
+}
