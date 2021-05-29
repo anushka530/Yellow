95f697e94c67199ac0d65184a4573ebc94017b17
anushka530 <shriyamanushka@gmail.com>
2021-05-28 21:49:04 -0400

    new commit

---
+++ app/src/main/java/com/example/aasthaapp/Fragments/THERAPIST_LIST.java
@@ -0,0 +1,66 @@
+package com.example.aasthaapp.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aasthaapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link THERAPIST_LIST#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class THERAPIST_LIST extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public THERAPIST_LIST() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment THERAPIST_LIST.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static THERAPIST_LIST newInstance(String param1, String param2) {
+        THERAPIST_LIST fragment = new THERAPIST_LIST();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_t_h_e_r_a_p_i_s_t__l_i_s_t, container, false);
+    }
+}
